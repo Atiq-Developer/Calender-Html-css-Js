4d0b8a8e060d5d0cfe187eedfc66b9101f38ac5c
Muhammad Atiq Ur Rehman <146697001+Atiq-Developer@users.noreply.github.com>
2024-06-29 23:46:34 +0500

    Add files via upload

---
+++ Calender/script.js
@@ -0,0 +1,67 @@
+const header = document.querySelector(".calender h3");
+const dates = document.querySelector(".dates");
+const navs = document.querySelectorAll("#prev, #next");
+
+const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+];
+let date = new Date();
+let month = date.getMonth();
+let year = date.getFullYear();
+
+function renderCalender() {
+    const start = new Date(year, month, 1).getDay();
+    const endDate = new Date(year, month + 1, 0).getDate();
+    const end = new Date(year, month, endDate).getDay();
+    const endDatePrev = new Date(year, month, 0).getDate();
+
+    let datesHTML = "";
+
+    for (let i = start; i > 0; i--) {
+        datesHTML += `<li class="inactive">${endDatePrev - i + 1}</li>`;
+    }
+    for (let i = 1; i <= endDate; i++) {
+        let className = i === date.getDate() && month === new Date().getMonth() &&
+        year === new Date().getFullYear() ? ' class="today"' : "";
+        datesHTML += `<li${className}>${i}</li>`;
+    }
+    for (let i = end; i < 6; i++) {
+        datesHTML += `<li class="inactive">${i - end + 1}</li>`;
+    }
+
+    dates.innerHTML = datesHTML;
+    header.textContent = `${months[month]} ${year}`;
+}
+
+navs.forEach((nav) => {
+    nav.addEventListener("click", (e) => {
+        const btnID = e.target.id;
+        if (btnID === "prev" && month === 0) {
+            year--;
+            month = 11;
+        } else if (btnID === "next" && month === 11) {
+            year++;
+            month = 0;
+        } else {
+            month = btnID === "next" ? month + 1 : month - 1;
+        }
+        date = new Date(year, month, new Date().getDate());
+        year = date.getFullYear();
+        month = date.getMonth();
+
+        renderCalender();
+    });
+});
+
+renderCalender();
